b5940cc135d9d3933477f907d4bdbbf8f78608ae
Sergei Anikeev <anikeevsu@gmail.com>
2025-05-06 14:06:09 +1200

    IAsyncLifetime

--- 3. Fundamentals/tests/TestProject2/CustomerControllerTests.cs
+++ 3. Fundamentals/tests/TestProject2/CustomerControllerTests.cs
@@ -2,22 +2,49 @@ using System.Net;
 
 namespace Customers.Api.Tests.Integration
 {
-    public class CustomerControllerTests
+    public class CustomerControllerTests : IAsyncLifetime
     {
+        private readonly HttpClient _httpClient = new()
+        {
+            BaseAddress = new Uri("https://localhost:5001")
+        };
+        public CustomerControllerTests()
+        {
+            //setup code
+        }
+        public Task InitializeAsync()
+        {
+            return Task.CompletedTask;
+        }
+        public async Task DisposeAsync()
+        {
+            await Task.Delay(1);
+        }
+
         [Fact]
         public async Task Get_ReturnNorFound_WhenCustomerDoesNotExist()
         {
             //Arrange
-            var httpClient = new HttpClient
-            {
-                BaseAddress = new Uri("https://localhost:5001")
-            };
 
             //Act
-            var response = await httpClient.GetAsync($"/customers/{Guid.NewGuid()}");
+            var response = await _httpClient.GetAsync($"/customers/{Guid.NewGuid()}");
 
             //Assert
             Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
         }
+
+        [Fact]
+        public async Task Get_ReturnNorFound_WhenCustomerDoesNotExist2()
+        {
+            //Arrange
+
+            //Act
+            var response = await _httpClient.GetAsync($"/customers/{Guid.NewGuid()}");
+
+            //Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+
     }
 }
