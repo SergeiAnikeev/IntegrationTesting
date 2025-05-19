9d8f795c4403fb1ae80858e4c37aba9fbaa2ec00
Sergei Anikeev <anikeevsu@gmail.com>
2025-05-19 14:23:35 +1200

    CustomerControllerTests folder

--- 3. Fundamentals/tests/TestProject2/CustomerController/CreateCustomerControllerTests.cs
+++ 3. Fundamentals/tests/TestProject2/CustomerController/CreateCustomerControllerTests.cs
@@ -1,15 +1,14 @@
-using Bogus;
+﻿using Bogus;
 using Customers.Api.Contracts.Requests;
 using Customers.Api.Contracts.Responses;
 using FluentAssertions;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net;
 using System.Net.Http.Json;
 
-namespace Customers.Api.Tests.Integration
+namespace Customers.Api.Tests.Integration.CustomerController
 {
-    public class CustomerControllerTests : IClassFixture<WebApplicationFactory<IApiMarker>>, IAsyncLifetime
+    public class CreateCustomerControllerTests : IClassFixture<WebApplicationFactory<IApiMarker>>, IAsyncLifetime
     {
         private readonly HttpClient _httpClient;
         private readonly Faker<CustomerRequest> _customerGenerator = new Faker<CustomerRequest>()
@@ -20,25 +19,11 @@ namespace Customers.Api.Tests.Integration
             ;
         private readonly List<Guid> _createdIds = new();
 
-        public CustomerControllerTests(WebApplicationFactory<IApiMarker> appFactory)
+        public CreateCustomerControllerTests(WebApplicationFactory<IApiMarker> appFactory)
         {
             _httpClient = appFactory.CreateClient();
         }
 
-        [Fact]
-        public async Task Get_ReturnNorFound_WhenCustomerDoesNotExist()
-        {
-            //Act
-            var response = await _httpClient.GetAsync($"/customers/{Guid.NewGuid()}");
-
-            //Assert
-            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
-            //var text = await response.Content.ReadAsStringAsync();
-            var problem = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
-            problem!.Title.Should().Be("Not Found");
-            problem.Status.Should().Be(404);
-
-        }
 
         [Fact]
         public async Task Create_ReturnsCreated_WhenCustomerIsCreated()
@@ -61,10 +46,11 @@ namespace Customers.Api.Tests.Integration
 
         public async Task DisposeAsync()
         {
-            foreach(var createdId in _createdIds) 
+            foreach (var createdId in _createdIds)
             {
                 await _httpClient.DeleteAsync($"/customers/{createdId}");
             }
         }
     }
 }
+
---
+++ 3. Fundamentals/tests/TestProject2/CustomerController/GetCustomerControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace Customers.Api.Tests.Integration.CustomerController
+{
+    public class GetCustomerControllerTests : IClassFixture<WebApplicationFactory<IApiMarker>>
+    {
+        private readonly HttpClient _httpClient;
+        public GetCustomerControllerTests(WebApplicationFactory<IApiMarker> appFactory)
+        {
+            _httpClient = appFactory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Get_ReturnNorFound_WhenCustomerDoesNotExist()
+        {
+            //Act
+            var response = await _httpClient.GetAsync($"/customers/{Guid.NewGuid()}");
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+            //var text = await response.Content.ReadAsStringAsync();
+            var problem = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+            problem!.Title.Should().Be("Not Found");
+            problem.Status.Should().Be(404);
+
+        }
+    }
+}
